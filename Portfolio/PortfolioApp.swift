23dd47da7d2fc6e08b7d4cacebe3879e6423fa39
agmcoder <agmcoder@gmail.com>
2022-04-24 21:44:04 +0200

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,20 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Alejandro Gómez Martín on 24/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
