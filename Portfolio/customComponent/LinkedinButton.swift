2c94d592a87309cfcf152d83d4ff0ceeaba714f3
alejandro Gómez Martín <agmcoder@gmail.com>
2023-01-30 09:44:29 +0100

    showing sheet when click github profile and linkeding profile is ok

--- Portfolio/customComponent/LinkedinButton.swift
+++ Portfolio/customComponent/LinkedinButton.swift
@@ -11,13 +11,13 @@ struct LinkedinButton: View {
     
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.width) var width
+    @State var isShowingSheet = false
     
     var body: some View {
         
         Button(
             action:{
-                guard let url = URL(string: ConfigurationUrlEnumString.linkedinProfile.url) else { return }
-                UIApplication.shared.open(url)
+                isShowingSheet = true
             }
         ){
             Image("linkedin")
@@ -25,6 +25,9 @@ struct LinkedinButton: View {
                 .scaledToFit()
                 .frame( width: width * 0.2, height: width * 0.2)
                 .padding(5)
+        }
+        .sheet(isPresented: $isShowingSheet){
+            SFSafariViewWrapper(url: URL(string: ConfigurationUrlEnumString.linkedinProfile.url)!)
         }
     }
 }
